431c85092e7b7452f2d45d18a3a0e736e9bb94f5
dust <destiny2013@yeah.net>
2019-03-08 12:01:52 +0800

    add a new class

--- python_file/file01.py
+++ python_file/file01.py
@@ -1,3 +1,19 @@
 #This is a simple file
 
 print('Hello python')
+
+class player:
+    def __init__(self, name, age, num):
+        self.name = name
+        self.age = age
+        self.num = num
+
+kobe = player('Kobe', 30, 24)
+
+print(kobe.name)
+
+print(kobe.age)
+
+print(kobe.num)
+
+
